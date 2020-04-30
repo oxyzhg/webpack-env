9594e6ae5f33e07d874fcee47c3b7b3ba3649c90
kq <oxyzhg@163.com>
2020-04-30 15:53:27 +0800

    update webpack configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,12 @@ const ERROR = 'error';
 module.exports = _.merge(require('@dotlim/eslint-config').eslint, {
   rules: {
     'no-console': OFF,
-    'no-underscore-dangle': OFF,
     'no-cond-assign': WARN,
+    'no-nested-ternary': OFF,
     'no-plusplus': WARN,
     'no-trailing-spaces': ERROR,
     'no-restricted-syntax': OFF,
+    'no-underscore-dangle': OFF,
     'prefer-const': OFF,
     'no-param-reassign': OFF,
   },
