61957be0d651ebc1d2928e6a60024f814fc4e862
kq <oxyzhg@163.com>
2020-05-03 16:26:05 +0800

    format eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,14 @@ const ERROR = 'error';
 
 module.exports = _.merge(require('@dotlim/eslint-config').eslint, {
   rules: {
-    'no-console': OFF,
-    'no-underscore-dangle': OFF,
     'no-cond-assign': WARN,
-    'no-plusplus': WARN,
-    'no-trailing-spaces': ERROR,
-    'no-restricted-syntax': OFF,
-    'prefer-const': OFF,
+    'no-console': OFF,
     'no-param-reassign': OFF,
+    'no-plusplus': WARN,
+    'no-unused-vars': WARN,
+    'no-restricted-syntax': OFF,
+    'no-trailing-spaces': ERROR,
+    'no-underscore-dangle': OFF,
+    'prefer-const': OFF,
   },
 });
